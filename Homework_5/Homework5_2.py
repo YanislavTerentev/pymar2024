c118728e754921e3c743eda76b19edd7e23759de
Yanislav Terentev <yanislav.terentev.99@mail.ru>
2024-05-15 09:31:55 +0300

    Fix too long line

--- Homework_5/Homework5_2.py
+++ Homework_5/Homework5_2.py
@@ -3,12 +3,12 @@ import re
 
 
 def check_input(string):
-    """This function checks user input for letters and spaces and returns True or False"""
+    """Check user input for letters and spaces"""
     return re.fullmatch(r'[A-Za-z\s]*', string)
 
 
 def add_ing(string):
-    """This function splits string and adds 'ing' to the words"""
+    """Split string and adds 'ing' to the words"""
     sentence = []
     words = string.split()
     for word in words:
@@ -18,13 +18,13 @@ def add_ing(string):
 
 def main():
     """This is the main function"""
-    user_input = input('Enter your word or phrase(only english letters and spaces): ')
+    user_input = input('Enter your word or phrase(only letters and spaces): ')
     valid_user_input = check_input(user_input)
     if valid_user_input:
         end = add_ing(user_input)
         print(end)
     else:
-        print('Sorry, you input incorrect. Use only english letters and spaces.')
+        print('Use only english letters and spaces, please')
 
 
 if __name__ == "__main__":
