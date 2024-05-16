24515c595be0e9025e78fecdfeab27a9c2d06b53
Yanislav Terentev <yanislav.terentev.99@mail.ru>
2024-05-17 02:04:01 +0300

    fix pylint and pycodestyle errors (E501,W291,W105)

--- Homework_6/homework.py
+++ Homework_6/homework.py
@@ -1,40 +1,37 @@
 """My homework exercise"""
 # Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
-first_string = "Robin Singh"  # pylint: disable=invalid-name
-print(first_string.split())
+s_1 = "Robin Singh"  # pylint: disable=invalid-name
+print(s_1.split())
 
 
-'''
-"I love arrays they are my favorite" =>
-["I", "love", "arrays", "they", "are", "my", "favorite"]
-'''
-second_string = "I love arrays they are my favorite"  # pylint: disable=invalid-name
-print(second_string.split())
+# I love arrays they are my favorite" =>
+# ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+s_2 = "I love arrays they are my favorite"  # pylint: disable=invalid-name
+print(s_2.split())
 
 
-'''
-Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
-Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
-'''
-name_list = ["Ivan", "Ivanou"]
+# Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
+# Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
+
+data = ["Ivan", "Ivanou"]
 city = 'Minsk'  # pylint: disable=invalid-name
 country = 'Belarus'  # pylint: disable=invalid-name
-print(f"Привет, {name_list[0]} {name_list[1]}! Добро пожаловать в {city} {country}")
+print(f"Привет, {data[0]} {data[1]}! Добро пожаловать в {city} {country}")
 
 
-''' 
-Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
-сделайте из него строку => "I love arrays they are my favorite"
-'''
+# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# сделайте из него строку => "I love arrays they are my favorite"
+
 array = ["I", "love", "arrays", "they", "are", "my", "favorite"]
 print(" ".join(array))
 
-'''
-Создайте список из 10 элементов, вставьте на 3-ю
-позицию новое значение, удалите элемент из списка под индексом 6
-'''
 
-my_list = ["Hi", "World", "13", "Beautiful", "25", "48", "Name", "Sun", "77", "3"]
+# Создайте список из 10 элементов, вставьте на 3-ю
+# позицию новое значение, удалите элемент из списка под индексом 6
+
+
+my_list = ["Hi", "World", "13", "Fly", "25", "48", "Name", "Sun", "77", "3"]
 my_list[3] = "This is replace"
 del my_list[6]
 print(my_list)
