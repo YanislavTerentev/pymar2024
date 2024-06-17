cb49254424bb34da6c211589cfa6bde5fbd8ee38
Yanislav Terentev <yanislav.terentev.99@mail.ru>
2024-06-17 09:49:43 +0300

    return bool instead of string in functions

--- Homework/Homework_11/Library.py
+++ Homework/Homework_11/Library.py
@@ -16,44 +16,58 @@ class User:
     def reserve_book(self, book):
         if book.is_reserved:
             if book.username_take == self.username:
-                return 'You are already reserved this book'
-            return 'Book is already reserved'
+                print('You are already reserved this book')
+                return False
+            print('Book is already reserved')
+            return False
         if book.is_taken:
             if book.username_take == self.username:
-                return 'You are already taken this book'
-            return 'Book is already taken'
-
+                print('You are already taken this book')
+                return False
+            print('Book is already taken')
+            return False
         book.is_reserved = True
         book.username_take = self.username
-        return 'Book is reserved by: ' + self.username
+        print('Book is reserved by: ' + self.username)
+        return True
 
     def take_book(self, book):
         if book.is_taken:
             if book.username_take == self.username:
-                return 'You are already taken this book'
-            return 'Book is already taken'
+                print('You are already taken this book')
+                return False
+            print('Book is already taken')
+            return False
         if book.is_reserved:
             if book.username_take == self.username:
                 book.is_reserved = False
                 book.is_taken = True
-                return 'Book is yours: ' + self.username
-            return 'Book is already reserved'
+                print('Book is yours: ' + self.username)
+                return True
+            print('Book is already reserved')
+            return False
         book.is_taken = True
         book.username_take = self.username
-        return 'Book is yours: ' + self.username
+        print('Book is yours: ' + self.username)
+        return True
 
     def return_book(self, book):
         if book.is_taken:
             if book.username_take == self.username:
                 book.is_taken = False
                 book.username_take = None
-                return 'Thank you for returning this book'
-            return 'It\'s not your book'
+                print('Thank you for returning this book')
+                return True
+            print('It\'s not your book')
+            return False
         if book.is_reserved:
             if book.username_take == self.username:
-                return 'You cannot return book that is reserved by you'
-            return 'You cannot return book reserved by another user'
-        return 'You cannot return book that was not taken'
+                print('You cannot return book that is reserved by you')
+                return False
+            print('You cannot return book reserved by another user')
+            return False
+        print('You cannot return book that was not taken')
+        return False
 
 
 book1 = Book('Fahrenheit 451', 'Ray Bradbury', 224, '9780307475312')
@@ -64,6 +78,6 @@ book4 = Book('Dead City', 'Joe McKinney', 288, '9785171598236')
 user1 = User('Steve Smith')
 user2 = User('John Silver')
 
-assert user1.reserve_book(book1) == 'Book is reserved by: Steve Smith'
-assert user1.take_book(book1) == 'Book is yours: Steve Smith'
-assert user2.return_book(book1) == 'It\'s not your book'
+assert user1.reserve_book(book1), "error"
+assert user1.take_book(book1), "error"
+assert not user2.return_book(book1), "error"
